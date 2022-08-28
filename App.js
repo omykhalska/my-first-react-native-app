ed298507461778519814e19cc888da69da8b3995
omykhalska <omykhalska@gmail.com>
2022-08-29 01:44:51 +0300

    add fonts

--- App.js
+++ App.js
@@ -1,31 +1,98 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-
-const image = { uri: 'https://reactjs.org/logo-og.png' };
+import { useState, useCallback, useEffect } from 'react';
+import {
+  Alert,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'DynaPuff-Regular': require('./assets/fonts/DynaPuff-Regular.ttf'),
+    'DynaPuff-Bold': require('./assets/fonts/DynaPuff-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const nameHandler = text => setName(text);
+  const passwordHandler = text => setPassword(text);
+  const onLogin = () => {
+    Alert.alert('Credentials', `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <Text style={styles.text}>Hello world!</Text>
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <TextInput
+            value={name}
+            onChangeText={nameHandler}
+            placeholder="Username"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button title={'Login'} style={styles.input} onPress={onLogin} />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  image: {
-    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'Roboto-Regular',
+    ...Platform.select({
+      ios: {
+        backgroundColor: '#ff0000',
+      },
+      android: {
+        backgroundColor: '#00ff00',
+      },
+    }),
   },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+    fontFamily: 'DynaPuff-Regular',
   },
 });
